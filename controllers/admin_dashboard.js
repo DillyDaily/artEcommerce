66139e7df2ab2a55e921113c472f314b0bdcc5d8
rahul4times <tkdmaster.sharma@gmail.com>
2017-10-31 11:42:29 -0700

    admin changes

---
+++ controllers/admin_dashboard.js
@@ -0,0 +1,16 @@
+const knex = require("../db/knex.js");
+
+module.exports = {
+
+  // This will bring to admin dashboard page - Rahul
+  dashboard: function(req, res){
+    render('admin_dashboard');
+  },
+
+  // This will display all products on dashboard - Rahul
+
+  all
+
+
+
+}
