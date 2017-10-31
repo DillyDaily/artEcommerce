88c59ddf393dc7e27b9c2a7296c67df412302a00
Rod <LourdesMarieWalker@gmail.com>
2017-10-31 09:43:50 -0700

    creating routes and register_login.js

--- config/routes.js
+++ config/routes.js
@@ -5,12 +5,97 @@ const home = require("../controllers/home.js");
 const product = require("../controllers/product.js");
 const register_login = require("../controllers/register_login.js");
 const shoppingCart = require("../controllers/shoppingCart.js");
+const admin = require("../controllers/admin.js");
+const admin_login = require("../controllers/admin.js");
+const admin_dashboard = require("../controllers/admin_dashboard.js");
+const admin_product_dashboard = ("../controllers/admin_product_dashboard.js");
+const admin_product_category = ("../controllers/admin_product_category.js")
+
+
 module.exports = function(app){
 
-  app.get('/', customer.index);
+  app.get('/', home.index);
+
+  app.get('/products', products.getAll);
+
+  app.get('/about', about.getAll);
+
+  app.get('/contact', contact.getAll);
+
+  app.get('/register_login', register_login.login);  // CUSTOMER LOGIN
+
+  app.post('/register_login', register_login.check);  // CHECK CUSTOMER
+
+  app.post('/register_login/register', register_login.register);  // REGISTER NEW CUSTOMER
+
+  app.get('/admin_login', admin.login);
+
+  app.post('/admin_login', admin.check);
+
+  // EVERYTHING BELOW THIS LINE IS PROTECTED
+  app.use(userAuth);
+
+  app.get('/contact/:id', contact.getOne);
+
+  app.get('/deleteContact/:id', contact.delete);
+
+  app.get('/shoppingCart/:id', shoppingCart.getOne);
+
+  // EVERYTHING BELOW THIS LINE IS ADMIN PROTECTED
+  app.use(adminAuth);
+
+  // ADMIN DASHBOARD
+  app.get('/admin_dashboard', admin.dashboard);
+
+  // CATEGORIES
+  app.get('/admin_product_category', admin.categoryGetAll);
+
+  app.get('/admin_product_category/:id', admin.categoryGetOne);
+
+  app.post('/admin_product_category', admin.create);
+
+  app.get('/editAdmin_product_category/:id', admin.categoryEdit);
+
+  app.post('/editAdmin_product_category/:id', admin.categoryUpdate);
+
+  app.get('/deleteAdmin_product_category/:id', admin.categoryDelete);
+
+  // PRODUCT DASHBOARD
+  app.get('/admin_product_dashboard', product.getAll);
+
+  // PRODUCTS
+  app.get('/admin_product/:id', product.getOne);
+
+  app.get('/admin_product/add', product.addOne);
+
+  app.post('/admin_product/add', product.create);
+
+  app.get('/editProduct/:id', product.edit);
+
+  app.post('/editProduct/:id', product.update);
+
+  app.get('/deleteProduct/:id', product.delete);
+
+  // USER SESSION FUNCTION
+  function userAuth(req, res, next){
+    if(req.session.user || req.session.admin){
+      next();
+    }else{
+      res.redirect("/users/login")
+    }
+  }
+
+  // ADMIN SESSION FUNCTION
+  function adminAuth(req,res,next){
+    if(req.session.admin){
+      next();
+    }else{
+      res.redirect('/login');
+    }
+  }
 
   app.use(function(req, res){
-    
+
       res.render('404');
     })
   }
