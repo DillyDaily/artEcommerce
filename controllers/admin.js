66333f4cb137b35c4835256d591bd095d510370c
rahul4times <ams@Rahuls-MacBook-Air.local>
2017-10-31 11:29:21 -0700

    admin login fixed

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,11 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-  // This will take admin to admin login page
+  // This will take admin to admin login page - Rahul
   login: function(req, res){
-
     res.render('admin_login', {message: req.session.message});
   },
-  // This will check admin email and password
+  // This will check admin email and password - Rahul
   check: function(req, res){
     knex('admin')
       .where('email', req.body.email)
@@ -17,16 +16,25 @@ module.exports = {
         encryption.check(user, req.body).then((isValid)=>{
           if(isValid){
             req.session.admin = admin.id;
-            res.redirect('/admin_dashboard');
+            req.session.save(()=>{
+              res.redirect('/admin_dashboard');
+            });
+
           }else{
-            req.session.message = "You entered an invalid username or password.";
-            res.redirect('/admin_login');
+            req.session.message = "You entered an invalid email or password.";
+            req.session.save(()=>{
+              res.redirect('/admin_login');
+            })
+
           }
         })
       })
       .catch((err)=>{
-        req.session.message = "You entered an invalid username or password."
-        res.redirect('/admin_login')
+        req.session.message = "You entered an invalid email or password."
+        req.session.save(()=>{
+          res.redirect('/admin_login');
+        })
+
       })
   }
 
--- controllers/admin_dashboard.js
+++ controllers/admin_dashboard.js
@@ -1,11 +1,15 @@
 const knex = require("../db/knex.js");
 
 module.exports = {
-  // This will bring to admin dashboard page
 
+  // This will bring to admin dashboard page - Rahul
   dashboard: function(req, res){
     render('admin_dashboard');
-  }
+  },
+
+  // This will display all products on dashboard - Rahul
+
+  all
 
 
 
