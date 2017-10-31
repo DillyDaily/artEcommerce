6a4fc519a5097ecce96f21d44d806dfe4b9c5025
rahul4times <tkdmaster.sharma@gmail.com>
2017-10-31 15:25:36 -0700

    edit server.js

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 const port = process.env.PORT || 8000;
 const methodOverride = require("method-override");
 
-app.use(express.static('static'));
+app.use(express.static('public'));
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
 app.set('view engine', 'ejs');
-
+require("./config/sessions.js")(app);
 var routes_setter = require('./config/routes.js');
 routes_setter(app);
 
