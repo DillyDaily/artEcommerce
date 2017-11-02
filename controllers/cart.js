cb307d6cb7d97c903b6f35e25aa230e766ae0231
Marie Walker <LourdesMarieWalker@gmail.com>
2017-11-02 11:06:30 -0700

    cart

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,10 +3,6 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   cartPage: function(req, res) {
-    if(!req.session.cart) {
-      req.session.cart = [];
-    }
-    console.log(res.session.cart);
     res.render("cart", {cartObj: res.session.cart});
   },
 
