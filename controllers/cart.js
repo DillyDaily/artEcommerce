5526ed7c873086d89960fb099fbe158492da9da8
Marie Walker <LourdesMarieWalker@gmail.com>
2017-11-02 12:26:52 -0700

    updating shopping cart table

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,7 +3,11 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   cartPage: function(req, res) {
-    res.render("cart", {cartObj: res.session.cart});
+    knex('product')
+      .then((pdtResult) => {
+        res.render("cart", {cartObj: req.session.cart, pdt: pdtResult});
+      });
+
   },
 
   addToCart: function(req, res) {
