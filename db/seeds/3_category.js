442ebc03c0c847b18c8ee90d903c745fccc079db
Alexis <ali.finney@gmail.com>
2017-11-02 18:53:59 -0700

    deleted ids from seed files

--- db/seeds/3_category.js
+++ db/seeds/3_category.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     knex('category').del(),
 
     // Inserts seed entries
-    knex('category').insert({id: 1, category_name: "print", category_description: "prints of original artwork"}),
-    knex('category').insert({id: 2, category_name: "painting", category_description: "oil painting on canvas"})
+    knex('category').insert({category_name: "print", category_description: "prints of original artwork"}),
+    knex('category').insert({category_name: "painting", category_description: "oil painting on canvas"})
 
   );
 };
--- db/seeds/7_order_status.js
+++ db/seeds/7_order_status.js
@@ -5,9 +5,9 @@ exports.seed = function(knex, Promise) {
     knex('order_status').del(),
 
     // customer_ide ,d 7, category_id:1}),
-    knex('order_status').insert({id: 1, order_id: 1, status_processed: true, status_shipped: false}),
-    knex('order_status').insert({id: 2, order_id: 2, status_processed: false, status_shipped: false}),
-    knex('order_status').insert({id: 3, order_id: 3, status_processed: true, status_shipped: true})
+    knex('order_status').insert({order_id: 1, status_processed: true, status_shipped: false}),
+    knex('order_status').insert({order_id: 2, status_processed: false, status_shipped: false}),
+    knex('order_status').insert({order_id: 3, status_processed: true, status_shipped: true})
 
   );
 };
