cfc3c1b6d73ff7e1004aa5dcf2cafb24e84d4aa8
Marie Walker <LourdesMarieWalker@gmail.com>
2017-11-04 10:42:01 -0700

    edit product.js

--- controllers/product.js
+++ controllers/product.js
@@ -85,7 +85,7 @@ module.exports = {
         category_id: req.body.category_id
 
         // sales_tax: req.body.tax
-      }, )
+      }),
       .then((result)=>{
         res.redirect('/admin_product_dashboard');
       })
